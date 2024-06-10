62d5218a9c5b1c04e8c83e5956aba82b43975363
Zeyu Chen <iszeyu.chen@gmail.com>
2024-06-10 16:17:05 +1000

    fix: fix bugs

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -16,10 +16,10 @@ export const revalidate = 0;
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
   return (
-    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+    <div className="h-full w-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
       <Header className="from-bg-neutral-900">
         <div className="mb-2 flex flex-col gap-y-6">
-          <h1 className="text-white text-3xl font-semibold">Search</h1>
+          <h1 className="text-3xl font-semibold text-white">Search</h1>
           <SearchInput />
         </div>
       </Header>
