7509a767b641ae2186bf1332d9eff61b55dcbde7
Zeyu Chen <iszeyu.chen@gmail.com>
2024-06-20 02:06:31 +1000

    fix: fix supabase domain bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["enslclfxdhezohzcesvn.supabase.co"]
+    domains: ["dexsbnufasucyoabbxmd.supabase.co"]
   }
 };
 
