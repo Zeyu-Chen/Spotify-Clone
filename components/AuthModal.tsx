39d344a90890d1d52048d6155bbe6ab2a4d69001
Zeyu Chen <iszeyu.chen@gmail.com>
2024-06-20 06:08:05 +1000

    feat: add google auth

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -38,7 +38,7 @@ const AuthModal = () => {
     >
       <Auth
         theme="dark"
-        providers={['github']}
+        providers={['github', 'google']}
         magicLink
         supabaseClient={supabaseClient}
         appearance={{
